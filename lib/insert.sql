441512465f502e3e870208bda89c122b0fb491d4
nathaniEL mujesia <nathanielmujesia@gmail.com>
2024-03-26 01:06:44 +0300

    Completed assignment

---
+++ lib/insert.sql
@@ -0,0 +1,17 @@
+-- - Mr. Chocolate
+-- - Rowdy
+-- - Tabitha
+-- - Sergeant Brown
+-- - Melissa
+-- - Grinch
+-- - Wendy
+-- - unnamed (refer back to how to create a record that doesn't have one value)
+
+INSERT INTO bears (name,age,sex,color,temperament,alive) VALUES ('Mr. Chocolate',6,'M','brown','melancholic',1);
+INSERT INTO bears (name,age,sex,color,temperament,alive) VALUES ('Rowdy',4,'M','red','phlegmatic',1);
+INSERT INTO bears (name,age,sex,color,temperament,alive) VALUES ('Tabitha',5,'F','black','sanguine',1);
+INSERT INTO bears (name,age,sex,color,temperament,alive) VALUES ('Sergeant Brown',7,'M','brown','choleric',1);
+INSERT INTO bears (name,age,sex,color,temperament,alive) VALUES ('Melissa',1,'F','white','choleric',1);
+INSERT INTO bears (name,age,sex,color,temperament,alive) VALUES ('Grinch',2,'M','green','phlegmatic',1);
+INSERT INTO bears (name,age,sex,color,temperament,alive) VALUES ('Wendy',1,'F','red','sanguine',1);
+INSERT INTO bears (name,age,sex,color,temperament,alive) VALUES (NULL,3,'M','black','melancholic',1);
